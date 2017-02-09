1181a87b3d81a9af7a3301815885f4e100b37ec4
kethan_dasari <kethansrinivas9@gmail.com>
2017-02-09 21:45:01 +0530

    KD:added permissions for the app successfully

--- app/src/main/java/com/kitspl/kh2139/lyricsmusic/MainActivity.java
+++ app/src/main/java/com/kitspl/kh2139/lyricsmusic/MainActivity.java
@@ -9,6 +9,7 @@ import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,34 +28,49 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestPermissions();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        lv = (ListView)findViewById(R.id.playList);
-        final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
-        final String[] songNames = new String[ mySongs.size() ];
-        for(int i=0;i<mySongs.size();i++) {
-            //toast(mySongs.get(i).getName());
-            songNames[i] = mySongs.get(i).getName().replace(".mp3", "");
+        try {
+            requestPermissions();
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
         }
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.song_layout,R.id.songName,songNames);
-            lv.setAdapter(arrayAdapter);
-            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    startActivity(new Intent(getApplicationContext(),MusicPlayer.class).putExtra("pos",position).putExtra("songlist",mySongs)) ;
-                }
-            });
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void requestPermissions(){
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG, "Permission is granted");
-            }
-            else {
-                Log.v(TAG, "Permission is not granted");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted now fetch the data from the storage
+                    Toast.makeText(MainActivity.this, "External storage Read Permission granted", Toast.LENGTH_SHORT).show();
+                    lv = (ListView) findViewById(R.id.playList);
+                    final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+                    final String[] songNames = new String[mySongs.size()];
+                    for (int i = 0; i < mySongs.size(); i++) {
+                        //toast(mySongs.get(i).getName());
+                        songNames[i] = mySongs.get(i).getName().replace(".mp3", "");
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.song_layout, R.id.songName, songNames);
+                    lv.setAdapter(arrayAdapter);
+                    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            startActivity(new Intent(getApplicationContext(), MusicPlayer.class).putExtra("pos", position).putExtra("songlist", mySongs));
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
             }
         }
     }
